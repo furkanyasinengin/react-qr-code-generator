a76dc71fef4c89a803da8acd59e1950f5df6ea1d
Furkan Yasin Engin <furkanyasinengin@gmail.com>
2026-01-02 22:56:52 +0300

    feat: improve ui styling and fix empty url crash

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App = () => {
   const [secondaryColor, setSecondaryColor] = useState("#ff0000");
 
   useEffect(() => {
+    if (!url) return;
     const qrObject = getQRCode(url);
 
     const ctx = canvasRef.current?.getContext("2d");
@@ -37,19 +38,40 @@ const App = () => {
   }, [url, primaryColor, secondaryColor]);
 
   return (
-    <div>
-      <canvas ref={canvasRef} width={400} height={400}></canvas>
-      <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
-      <input
-        type="color"
-        value={primaryColor}
-        onChange={(e) => setPrimaryColor(e.target.value)}
-      />
-      <input
-        type="color"
-        value={secondaryColor}
-        onChange={(e) => setSecondaryColor(e.target.value)}
-      />
+    <div className="min-h-screen flex gap-2 bg-gray-100 justify-center items-center">
+      <div className="bg-white rounded-xl shadow-xl w-full max-w-md p-6">
+        <canvas
+          className="w-full h-auto border-2 border-gray-700/75 rounded p-1.5 mb-4"
+          ref={canvasRef}
+          width={400}
+          height={400}
+        />
+        <div className="flex flex-col gap-4 items-center">
+          <input
+            className="w-full border p-2 rounded font-mono focus:ring-2"
+            type="text"
+            value={url}
+            placeholder="Write Someting..."
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <div className="grid grid-cols-2 gap-x-3 items-center">
+            <label className="font-mono text-xl">Data Color:</label>
+            <input
+              className="w-12 h-12 p-1 border-0 rounded overflow-hidden cursor-pointer shadow-sm"
+              type="color"
+              value={primaryColor}
+              onChange={(e) => setPrimaryColor(e.target.value)}
+            />
+            <label className="font-mono text-xl">Eye Color:</label>
+            <input
+              className="w-12 h-12 p-1 border-0 rounded overflow-hidden cursor-pointer shadow-sm"
+              type="color"
+              value={secondaryColor}
+              onChange={(e) => setSecondaryColor(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
